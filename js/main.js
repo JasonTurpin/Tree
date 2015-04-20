0bc3b9d8debe754aee43ce0657956f0e05bd542b
JasonTurpin <JasonATurpin@gmail.com>
2015-04-19 20:06:06 -0400

    Add fuelux, tree view code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ require.config({
         jquery    : '../vendor/jquery/1.11.1/jquery-1.11.1.min',
         underscore: '../vendor/underscore/1.6.0/underscore-min',
         backbone  : '../vendor/backbone/1.1.2/backbone-min',
+        fuelux    : '../vendor/fuelux/js/tree.min',
         moment    : '../vendor/moment/moment',
         text      : '../vendor/require/text',
         bootstrap : '../vendor/bootstrap/dist/js/bootstrap.min',
@@ -14,6 +15,10 @@ require.config({
         bootstrap: {
             deps: ['jquery', 'underscore'],
             exports: 'bootstrap'
+        },
+        fuelux: {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'fuelux'
         }
     }
 });
