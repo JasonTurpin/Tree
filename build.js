340ebb899d09de0cf091575206ec4620ac2e41ce
JasonTurpin <JasonATurpin@gmail.com>
2015-04-20 11:07:59 -0400

    Activate minified version

--- build.js
+++ build.js
@@ -1,17 +1,24 @@
 ({
+    // Set paths
     paths: {
-        jquery           : '../vendor/jquery/1.11.1/jquery-1.11.1.min',
-        underscore       : '../vendor/underscore/1.6.0/underscore-min',
-        backbone         : '../vendor/backbone/1.1.2/backbone-min',
-        moment           : '../vendor/moment/moment',
-        text             : '../vendor/require/text',
-        bootstrap        : '../vendor/bootstrap/dist/js/bootstrap.min',
-        templates        : '../html/templates'
+        jquery    : '../vendor/jquery/1.11.1/jquery-1.11.1.min',
+        underscore: '../vendor/underscore/1.6.0/underscore-min',
+        backbone  : '../vendor/backbone/1.1.2/backbone-min',
+        fuelux    : '../vendor/fuelux/js/tree.min',
+        moment    : '../vendor/moment/moment',
+        text      : '../vendor/require/text',
+        bootstrap : '../vendor/bootstrap/dist/js/bootstrap.min',
+        templates : '../html/templates'
     },
+
     shim: {
         bootstrap: {
             deps: ['jquery', 'underscore'],
             exports: 'bootstrap'
+        },
+        fuelux: {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'fuelux'
         }
     },
     baseUrl                : "js",
